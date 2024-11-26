b2c0c00d090a92f77cc66c03baa93509e11f8460
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-11-26 14:19:46 -0300

    Implementing schema

---
+++ src/schema/product.ts
@@ -0,0 +1,19 @@
+import { gql } from "apollo-server";
+
+export const productSchema = gql`
+  type Product {
+    id: ID!
+    name: String!
+    price: Float!
+    stock: Int!
+  }
+
+  type Query {
+    product(id: ID!): Product
+    allProducts: [Product!]!
+  }
+
+  type Mutation {
+    deleteProduct(id: ID!): Boolean
+  }
+`;
