75fec03ce6bdfd75b2f70349d7785a5f4227edb3
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-11-26 22:48:16 -0300

    Improv the query

--- src/db/products.ts
+++ src/db/products.ts
@@ -1,4 +1,4 @@
-export const products = [
+const _products = [
   { id: "0", name: "Product A", price: 100, stock: 50 },
   { id: "1", name: "Product B", price: 200, stock: 40 },
   { id: "2", name: "Product C", price: 300, stock: 30 },
@@ -6,14 +6,14 @@ export const products = [
   { id: "4", name: "Product E", price: 500, stock: 10 },
 ];
 
-export const getProductById = (id: string) => products.find((p) => p.id === id);
+export const getProductById = (id: string) => _products.find((p) => p.id === id);
 
-// export const getProducts = () => products;
+export const getProducts = _products;
 
 export const deleteProductById = (id: string) => {
-  const index = products.findIndex((p) => p.id === id);
+  const index = _products.findIndex((p) => p.id === id);
   if (index > -1) {
-    return products.splice(index, 1)[0];
+    return _products.splice(index, 1)[0];
   }
   return null;
 };
